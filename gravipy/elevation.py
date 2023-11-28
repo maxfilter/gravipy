144e9e87c69af30e86399a6bc78f5c53655a504a
ayatzy <80324436+ayatzy@users.noreply.github.com>
2023-11-28 15:50:32 -0500

    fix a small command mistake
    updated squared command

--- gravipy/elevation.py
+++ gravipy/elevation.py
@@ -2,13 +2,13 @@
 import numpy as np
 import math
 
-G=6.67*10^(-11) #gravitational constant [N*m^2/kg^2]
-r_eq=6378.137*10^3 #equatorial radius [m]
+G=6.67*10**(-11) #gravitational constant [N*m^2/kg^2]
+r_eq=6378.137*10**3 #equatorial radius [m]
 rho_cr=2670 #density of crust [kg/m^3]
 
-def free_air_correction(g_n: np.ndarray, H: np.ndarray) -> np.ndarray:
+def free_air_correction(g_normal: np.ndarray, H: np.ndarray) -> np.ndarray:
 
-    dg_free = 2.*H*g_n/r_eq
+    dg_free = 2.*H*g_normal/r_eq
 
     return dg_free
 

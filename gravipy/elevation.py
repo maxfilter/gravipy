03c10ca1997be906a8a88ea401ae3d2aa4263690
ayatzy <80324436+ayatzy@users.noreply.github.com>
2023-11-28 15:39:45 -0500

    Update elevation.py
    1. added constants
    2. deleted latitude_correction
    3. updated free_air_correction and bouguer_plate_correction

--- gravipy/elevation.py
+++ gravipy/elevation.py
@@ -1,14 +1,19 @@
 """Elevation corrections for gravity data."""
 import numpy as np
+import math
 
+G=6.67*10^(-11) #gravitational constant [N*m^2/kg^2]
+r_eq=6378.137*10^3 #equatorial radius [m]
+rho_cr=2670 #density of crust [kg/m^3]
 
-def latitude_correction(g_measured: np.ndarray, lat: np.ndarray) -> np.ndarray:
-    return np.zeros_like(g_measured)
+def free_air_correction(g_n: np.ndarray, H: np.ndarray) -> np.ndarray:
 
+    dg_free = 2.*H*g_n/r_eq
 
-def free_air_correction(g_measured: np.ndarray) -> np.ndarray:
-    return np.zeros_like(g_measured)
+    return dg_free
 
+def bouguer_plate_correction(H: np.ndarray) -> np.ndarray:
 
-def bouguer_plate_correction(g_measured: np.ndarray) -> np.ndarray:
-    return np.zeros_like(g_measured)
+    dg_bp = 2.*math.pi*rho_cr*G*H
+
+    return dg_bp
